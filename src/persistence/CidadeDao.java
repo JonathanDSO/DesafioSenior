0f5642e6e99f03d29e4c911aa1a79b0fa90c588a
Jonathan Oliveira <jonathanso@outlook.com>
2017-07-22 22:58:13 -0300

    Verifica se Cidade já foi cadastrada. Caso já tenha sido cadastrada, a cidade é atualizada.

--- src/persistence/CidadeDao.java
+++ src/persistence/CidadeDao.java
@@ -36,24 +36,40 @@ public class CidadeDao extends Dao {
 
 			lista.add(cidade);
 		}
+		stmt.close();
 		close();
 		return lista;
+	}
+	
+	public Cidade findById(Integer ibge_id) throws Exception {
+		open();
+		stmt = con.prepareStatement("select * from cidade where ibge_id=?");
+		stmt.setInt(1, ibge_id);
+		rs = stmt.executeQuery();
+		Cidade cidade = null;
+		if (rs.next()) {
+			cidade = new Cidade(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getBoolean(4), rs.getDouble(5),
+					rs.getDouble(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10));
+		}
+		stmt.close();
+		close();
+		return cidade;
 	}
 
 	public void edit(Cidade c) throws Exception {
 		open();
 		stmt = con.prepareStatement(
-				"update cidade set ibge_id=?, uf=?, name=?, capital=?, lon=?, lat=?, no_accents=?, alternative_names=?, microregion=?, mesoregion=?");
-		stmt.setInt(1, c.getIbge_id());
-		stmt.setString(2, c.getUf());
-		stmt.setString(3, c.getName());
-		stmt.setBoolean(4, c.getCapital());
-		stmt.setDouble(5, c.getLon());
-		stmt.setDouble(6, c.getLat());
-		stmt.setString(7, c.getNo_accents());
-		stmt.setString(8, c.getAlternative_names());
-		stmt.setString(9, c.getMicroregion());
-		stmt.setString(10, c.getMesoregion());
+				"update cidade set uf=?, name=?, capital=?, lon=?, lat=?, no_accents=?, alternative_names=?, microregion=?, mesoregion=? where ibge_id=?");
+		stmt.setString(1, c.getUf());
+		stmt.setString(2, c.getName());
+		stmt.setBoolean(3, c.getCapital());
+		stmt.setDouble(4, c.getLon());
+		stmt.setDouble(5, c.getLat());
+		stmt.setString(6, c.getNo_accents());
+		stmt.setString(7, c.getAlternative_names());
+		stmt.setString(8, c.getMicroregion());
+		stmt.setString(9, c.getMesoregion());
+		stmt.setInt(10, c.getIbge_id());
 		stmt.executeUpdate();
 		stmt.close();
 		close();
--- src/resources/RestCidade.java
+++ src/resources/RestCidade.java
@@ -2,8 +2,6 @@ package resources;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import javax.ws.rs.Path;
@@ -18,9 +16,8 @@ public class RestCidade {
 	String caminhoArq = "WebContent/cidades.csv";
 	CidadeDao cidadeDao = new CidadeDao();
 
-	private List<Cidade> lerArquivoCSV() {
+	private void lerArquivoCSV() {
 		try {
-			List<Cidade> cidades = new ArrayList<Cidade>();
 			String linhas = new String();
 			File arquivoCSV = new File(caminhoArq);
 			leitor = new Scanner(arquivoCSV);
@@ -29,32 +26,41 @@ public class RestCidade {
 			while (leitor.hasNext()) {
 				linhas = leitor.nextLine();
 				String[] valores = linhas.split(",");
-				Cidade cidade = new Cidade(
-						new Integer(valores[0]),
-						valores[1],
-						valores[2], 
-						new Boolean(valores[3]), 
-						new Double(valores[4]),
-						new Double(valores[5]), 
-						valores[6], 
-						valores[7],
-						valores[8], 
-						valores[9]);
-				gravarCidade(cidade);
-				cidades.add(cidade);
+				Cidade cidade = new Cidade(new Integer(valores[0]), valores[1], valores[2], new Boolean(valores[3]),
+						new Double(valores[4]), new Double(valores[5]), valores[6], valores[7], valores[8], valores[9]);
+				
+				Cidade c = buscarCidadePorId(cidade.getIbge_id());
+				if(c != null){
+					atualizarCidade(cidade);
+				} else {
+					gravarCidade(cidade);
+				}
 			}
-			
-			return cidades;
-
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void gravarCidade(Cidade cidade) {
+		try {
+			cidadeDao.create(cidade);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private Cidade buscarCidadePorId(Integer ibge_id){
+		try {
+			return cidadeDao.findById(ibge_id);
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	private void gravarCidade(Cidade cidade){
+	private void atualizarCidade(Cidade cidade){
 		try {
-			cidadeDao.create(cidade);
+			cidadeDao.edit(cidade);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -62,6 +68,12 @@ public class RestCidade {
 
 	public static void main(String[] args) {
 		new RestCidade().lerArquivoCSV();
+		try {
+			System.out.println(new CidadeDao().findAll());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
